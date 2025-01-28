d8e674c7aa16242746d1c4667e7ff8ed51f0bec6
Bjørn Nachtwey <143603270+bnw4cristie@users.noreply.github.com>
2025-01-28 10:42:45 +0100

    Update set-PTB-Timeservers.bat

--- Windows/N2K/bin/set-PTB-Timeservers.bat
+++ Windows/N2K/bin/set-PTB-Timeservers.bat
@@ -1,1 +1,1 @@
-C:\WINDOWS\system32>w32tm /config /manualpeerlist:"ptbtime2.ptb.de,ptbtime4.ptb.de" /syncfromflags:manual /reliable:yes /update
+C:\WINDOWS\system32\w32tm /config /manualpeerlist:"ptbtime2.ptb.de,ptbtime4.ptb.de" /syncfromflags:manual /reliable:yes /update
