334237e55c48d92d2bd4087058d7f4feb58f5dc3
Bjørn Nachtwey <143603270+bnw4cristie@users.noreply.github.com>
2025-03-13 10:44:06 +0100

    Create CleanUP.ps1

---
+++ Windows/CleanUP.ps1
@@ -0,0 +1,6 @@
+# removing some *AppxPackages*
+Get-AppxPackage *YourPhone* | Remove-AppxPackage
+Get-AppxPackage *WindowsStore* | Remove-AppxPackage
+Get-AppxPackage *Windows.DevHome* | Remove-AppxPackage
+
+
